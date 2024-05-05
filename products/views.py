03b8b781bb85e5a51bfd700b618fc4051bfbffc5
alanyu <aalan50208tw@gmail.com>
2024-05-05 22:41:09 +0800

    Pyshop - Basic model & views

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+# /products -> index
+# URL
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html',{'products':products})
+    # return HttpResponse('Hello World!')
+def new(request):
+    return HttpResponse('New products!')
